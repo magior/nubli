b1571ff71ff7a3e45521b11dd86801a4af7db8e8
Gert-Jan Compagner <gert.jan.compagner@ing.nl>
2020-10-11 10:23:06 +0200

    Fix searching by mac address

--- src/lib/smartLockPeripheralFilter.ts
+++ src/lib/smartLockPeripheralFilter.ts
@@ -1,27 +1,28 @@
 import { PeripheralFilter } from "./peripheralFilter";
+import { SmartLock } from "./smartLock";
 
 export class SmartLockPeripheralFilter implements PeripheralFilter {
-    constructor(public name: string, public macAddress: string) {
+    constructor(public name: string, public macAddress?: string) {
     }
 
     handle(peripheral: import("@abandonware/noble").Peripheral): boolean {
         let data: Buffer = peripheral.advertisement.manufacturerData;
-        
-        if (data !== undefined && data !== null && data.length == 25) {
+
+        if (this.macAddress) {
+            return peripheral.address.replace(/-/g, ':')===this.macAddress.toLowerCase();
+        } else if (data!==undefined && data!==null && data.length==25) {
             let type: number = data.readUInt8(2);
             let dataLength: number = data.readUInt8(3);
-        
+
             // 0x02 == iBeacon
-            if (type == 2 && dataLength == 21) {
-                // let serviceUuid: string = data.slice(4, 20).toString('hex');
-                let macAddress: string = data.toString('hex').slice(-14).slice(0, -2);
-                console.log(macAddress);
-                return this.macAddress.replace(/:/g, '') === macAddress;
+            if (type==2 && dataLength==21) {
+                let serviceUuid: string = data.slice(4, 20).toString('hex');
+                return serviceUuid==SmartLock.NUKI_SERVICE_UUID;
             }
             return false;
         } else {
             let name: string = peripheral.advertisement.localName;
-            return name!==undefined && peripheral.advertisement.localName.slice(5)===this.name;
+            return name!==undefined && peripheral.advertisement.localName.slice(0, 5)==="Nuki_";
         }
 
     }
